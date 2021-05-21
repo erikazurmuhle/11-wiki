201d12c092d18a7be24958d773a54de6ec978c9c
erikazurmuhle <erikadzurmuhle@gmail.com>
2021-05-21 15:37:15 -0300

    console

--- models/index.js
+++ models/index.js
@@ -2,8 +2,8 @@ const User = require("./Users");
 const Page = require("./Pages");
 
 Page.belongsTo(User, { as: "author" });
-
+console.log("hola");
 module.exports = {
-	User,
-	Page,
+  User,
+  Page,
 };
